f3b74e82b47b28c7621b782d7c7a994d7c673c3a
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-09 05:55:14 +0530

    ....

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4KRl-Ki8ZMQDx880WWHheEYS3OPeoyCU',
+  authDomain: 'dev-eloper',
+  projectId: 'developer-database-732',
+  storageBucket: 'gs://developer-database-732.appspot.com',
+  messagingSenderId: '232071454403',
+  appId: 'YOUR_APP_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Auth0Provider domain={authConfig.domain}
-  //   clientId={authConfig.clientId}
-  //   redirectUri={window.location.origin}>
-  // </Auth0Provider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
